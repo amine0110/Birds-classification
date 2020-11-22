7f296164cafd7c41f52726ca485e9081c3f51467
Mokhtari Mohammed El Amine <37108394+amine0110@users.noreply.github.com>
2020-11-22 22:10:57 +0100

    Update load_image.py

--- load_image.py
+++ load_image.py
@@ -12,35 +12,35 @@ def get_birds_names(path_to_folders):
 
 
  
-path_to_folders = '/home/pycad/Downloads/images/train'      # C'est le path pour les dossiers qui contienent les noms des oiseaux
-path_to_model = '/home/pycad/Documents/Project/code/model'  # C'est le path pour le dossier qui contient le modèle
+path_to_folders = 'The path where the folders that contain the birds names'      
+path_to_model = 'The path where you saved your model'  
 
 
-model = tf.keras.models.load_model(path_to_model)       # Dans cette ligne on a chargé le modèle
+model = tf.keras.models.load_model(path_to_model)       # Here we load the model
 
 def predict(path_of_new_image):
 
-    birds_names = get_birds_names(path_to_folders)          # On a appellé cette fonction pour remplir la liste des oiseaux
+    birds_names = get_birds_names(path_to_folders)          # We called the function that will fill our birds list
     
 
-    image_new_size = 229                                    # On a mis les nouvelles dimenstions de l'image à 299 parce qu'on a fait l'apprentissage avec des images de 229 pixels
-    img_array = cv2.imread(path_of_new_image)               # On a utilisé openCv pour ouvrir l'image parce qu'on a besoin d'une liste
+    image_new_size = 229                                    # We defined new sizes for the image because we did the trainig on images with 229 pixels
+    img_array = cv2.imread(path_of_new_image)               # We used openCv to read the image because we need it as an array
     img = img_array.copy()
-    img_array = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)        # Le soucis de openCv c'est qu'il ouvre les images avec des bandes inversée (BGR) donc il faut les inverser 
+    img_array = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)        # The issue with openCv is that it will reverse the channel when it open the image so we need to put every channel on its place as at the training
 
-    img_array = cv2.resize(img_array, (image_new_size, image_new_size)) # Cette ligne juste pour changer les dimensions de l'image avant de la traiter
-    img_array = img_array.astype("float32")                 # On a converti les valeurs d'image on des floats parce que dans la prochaine étape on va les diviser par 255 donc si on n'a pas des valeurs floats on aura que des 0 et 1
+    img_array = cv2.resize(img_array, (image_new_size, image_new_size)) # Here we gived the new dimensions to the image that we will predict
+    img_array = img_array.astype("float32")                 # Here we converted the pixel values to float because in the next step we will divide it by 255 so if we will let it intger all the values will be either 0 or 1 but we want it between 0 and 1
     
-    img_array = img_array / 255.0                           # On a fait le scalling parce que les réseaux de neurones fonctionent avec des valeurs normalisés alors il faut suivre l'apprentissage
-    np_image = np.expand_dims(img_array, axis=0)            # Dans cette ligne on a ajouté un bande à notre image (c'est du au nombre d'epoch dans l'apprentissage)
+    img_array = img_array / 255.0                           # Here we did the scalling as we did in the training because in the deep learning we have to use values between 0 and 1
+    np_image = np.expand_dims(img_array, axis=0)            # Here we added a channel to have much channel as the training(in the training there is a new channel for the indexe of the picture entred in our case we did just 2 images)
 
-    predictions = model(np_image)                           # Dans cette ligne on aura le résultat, le résultat sera un tableau des probabilités (30% oiseau A, 70% oiseau B, ...)
-    predicted_class_idx = np.argmax(predictions)            # Donc pour prendre le meilleur oiseau on va prendre l'indexe de la valeur max du proba
+    predictions = model(np_image)                           # As we know the prediction result will give us an array of probabilities so we will take the mas value which corespond to the right class
+    predicted_class_idx = np.argmax(predictions)            # We need to take the indexe of the max value which will help us to get the bird name from the names list
 
-    probability = np.max(predictions)                       # Et si on veut afficher sa probabilité on prend la valeur max directement
-    predicted_class = birds_names[predicted_class_idx]      # Pour avoir le nom d'oiseau il faut prendre l'indexe est retirer l'oiseaux dans la liste des oiseaux avec le meme indexe tout simplement
+    probability = np.max(predictions)                       # And here to take the probability value if we need to print it for example
+    predicted_class = birds_names[predicted_class_idx]      # This line will return the name of the bird selected
 
-    return predicted_class, probability                     # On a retourner une liste avec deux valeurs, le nom d'oiseau avec sa probabilité
+    return predicted_class, probability                     # We returned an array of two values, the bird name and its probability
 
 
 
