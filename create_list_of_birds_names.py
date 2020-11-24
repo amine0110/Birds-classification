f5f280f97befa6699724c080470d7be9d6ca08fc
Mokhtari Mohammed El Amine <37108394+amine0110@users.noreply.github.com>
2020-11-24 20:05:46 +0100

    Update create_list_of_birds_names.py

--- create_list_of_birds_names.py
+++ create_list_of_birds_names.py
@@ -1,5 +1,7 @@
 import os
 
-birds_names = os.listdir('/home/pycad/Downloads/images/train')
+# This function will help us to create the list of our birds using the folders of the images
+
+birds_names = os.listdir('The path of the folder that contains the classes (folders of images)')
 birds_names.sort()
 print(birds_names)
